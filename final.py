dc03f1b3fb2323d2212a4f833323703ffb9e30d8
NeoVander <neo9295@gmail.com>
2023-11-24 21:36:50 +0100

    Started the final interface

---
+++ final.py
@@ -0,0 +1,245 @@
+import nidaqmx
+from nidaqmx.constants import TerminalConfiguration, AcquisitionType
+import sys
+from PyQt6 import QtCore, QtWidgets, uic
+from PyQt6.QtCore import QTimer
+import pyqtgraph
+import numpy as np
+import pyvisa
+
+# Load the UI file created with QT Designer
+Ui_MainWindow, QtBaseClass = uic.loadUiType("Interface_finale.ui")
+
+
+# Main class for the interface
+class MyWindow(QtWidgets.QMainWindow, Ui_MainWindow):
+    data_acquired = QtCore.pyqtSignal(list)  # Signal to emit acquired data
+
+    # Initialize the interface
+    def __init__(self):
+        super(MyWindow, self).__init__()  # Initialize the parent class
+        self.setupUi(self)  # Load the UI
+        self.setWindowTitle("Interface de pilotage du FIB")  # Set window title
+
+        # Connect buttons to their respective functions
+        self.pushButton_quit.clicked.connect(self.quit)
+        self.pushButton_dev_refresh.clicked.connect(self.populate_dev_combobox)
+        self.pushButton_connect_dev.clicked.connect(self.connect_to_card)
+        self.pushButton_gpp_2323_refresh.clicked.connect(self.populate_gpp_2323_combobox)
+        self.pushButton_connect_gpp_2323.clicked.connect(self.connect_to_gpp_2323)
+        self.pushButton_connect_gpp_2323_help.clicked.connect(self.gpp_2323_help)
+        self.pushButton_gpp_4323_refresh.clicked.connect(self.populate_gpp_4323_combobox)
+        self.pushButton_connect_gpp_4323.clicked.connect(self.connect_to_gpp_4323)
+        self.pushButton_connect_gpp_4323_help.clicked.connect(self.gpp_4323_help)
+
+        # Connect sliders to their respective functions
+        self.gpp_2323_tension_slider.valueChanged.connect(self.gpp_2323_slider_changed)
+        self.gpp_4323_tension_slider.valueChanged.connect(self.gpp_4323_tension_slider_changed)
+        self.brightness_slider.valueChanged.connect(self.gpp_4323_brightness_slider_changed)
+
+        # Initialize instance variables
+        self.port_dev = None
+        self.port_gpp_2323 = None
+        self.port_gpp_4323 = None
+        self.gpp_2323 = None
+        self.gpp_4323 = None
+
+        # Initialize the comboBoxes
+        self.populate_dev_combobox()
+        self.populate_gpp_2323_combobox()
+        self.populate_gpp_4323_combobox()
+
+    # Function to quit the application
+    @staticmethod
+    def quit():
+        QtCore.QCoreApplication.instance().quit()
+
+    #########################################################################################
+    # NI connection part
+
+    # Function to display the current available devices
+    def populate_dev_combobox(self):
+        try:
+            system = nidaqmx.system.System.local()
+            items = [device.name for device in system.devices]  # Lists the connected NI devices
+            self.comboBox_dev.clear()  # Clear existing items
+            self.comboBox_dev.addItems(items)  # Add new items
+
+        except Exception as e:
+            QtWidgets.QMessageBox.information(self, 'Error', f"populate_dev_combobox function returned : {e}")
+
+    # Connection to the NI Card and verification
+    def connect_to_card(self):
+        try:
+            self.port_dev = self.comboBox_dev.currentText()
+            system = nidaqmx.system.System.local()
+            if (not self.comboBox_dev.currentText() in system.devices) or (
+                    self.comboBox_dev.currentText() == ""):  # Checks if the device is connected
+                QtWidgets.QMessageBox.information(self, 'Error', 'Wrong port choice')
+                self.populate_dev_combobox()
+                self.port_dev = None
+            else:  # If the device is connected, we do this
+                self.port_dev = self.comboBox_dev.currentText()
+                device = nidaqmx.system.Device(self.port_dev)
+                ao_channels = [chan.name for chan in
+                               device.ao_physical_chans]  # Lists the available analog output channels
+                ai_channels = [chan.name for chan in
+                               device.ai_physical_chans]  # Lists the available analog input channels
+                self.comboBox_ao.clear()  # Clear existing items
+                self.comboBox_ao.addItems(ao_channels)  # Add new items
+                self.comboBox_ai.clear()  # Clear existing items
+                self.comboBox_ai.addItems(ai_channels)  # Add new items
+
+        except Exception as e:
+            QtWidgets.QMessageBox.information(self, 'Error', f"Connect_to_card function returned : {e}")
+
+    #########################################################################################
+    # Gpp_2323 connection part
+
+    # Function to display the current available devices
+    def populate_gpp_2323_combobox(self):
+        try:
+            rm = pyvisa.ResourceManager()
+            items = rm.list_resources()  # Lists the connected VISA devices
+            self.comboBox_gpp_2323.clear()  # Clear existing items
+            self.comboBox_gpp_2323.addItems(items)  # Add new items
+
+        except Exception as e:
+            QtWidgets.QMessageBox.information(self, 'Error', f"populate_gpp_2323_combobox function returned : {e}")
+
+    # Connection to the Gpp_2323 and verification
+    def connect_to_gpp_2323(self):
+        try:
+            self.port_gpp_2323 = self.comboBox_gpp_2323.currentText()
+            rm = pyvisa.ResourceManager()
+            items = rm.list_resources()  # Lists the connected VISA devices
+            if (not self.comboBox_gpp_2323.currentText() in items) or (self.comboBox_gpp_2323.currentText() == ""):  # Checks if the device is connected
+                QtWidgets.QMessageBox.information(self, 'Error', 'Wrong choice')
+                self.populate_gpp_2323_combobox()
+                self.port_gpp_2323 = None
+            else:  # If the device is connected, we do this
+                self.port_gpp_2323 = self.comboBox_gpp_2323.currentText()
+                self.gpp_2323 = rm.open_resource(self.port_gpp_2323)
+                print(self.gpp_2323.query('*IDN?'))
+                try:
+                    self.gpp_2323_tension_slider.setEnabled(True)   # Allow the user to use the slider
+                    self.gpp_2323.write(f'ISET1:0.03')    # Fixes the maximum current so we can have a tension
+                    self.gpp_2323.write(f'ISET2:0.03')    # Fixes the maximum current so we can have a tension
+                    self.gpp_2323.write(f':ALLOUTON')  # Enables every channel
+                except Exception as e:
+                    QtWidgets.QMessageBox.information(self, 'Error', f"Couldn't connect to the instrument : {e}")
+                    self.gpp_2323_tension_slider.setEnabled(False)  # Prevent the user from using the slider
+                    self.populate_gpp_2323_combobox()
+                    self.port_gpp_2323 = None
+
+        except Exception as e:
+            QtWidgets.QMessageBox.information(self, 'Error', f"Connect_to_gpp_2323 function returned : {e}")
+
+    # Connection help button
+    def gpp_2323_help(self):
+        QtWidgets.QMessageBox.information(self, 'Help',
+                                          'Connect both CH1(-) and CH2(-) together and take your output between CH1(+) and CH2(+).')
+
+    #########################################################################################
+    # Gpp_4323 connection part
+
+    # Function to display the current available devices
+    def populate_gpp_4323_combobox(self):
+        try:
+            rm = pyvisa.ResourceManager()
+            items = rm.list_resources()  # Lists the connected VISA devices
+            self.comboBox_gpp_4323.clear()  # Clear existing items
+            self.comboBox_gpp_4323.addItems(items)  # Add new items
+
+        except Exception as e:
+            QtWidgets.QMessageBox.information(self, 'Error', f"populate_gpp_4323_combobox function returned : {e}")
+
+    # Connection to the gpp_4323 and verification
+    def connect_to_gpp_4323(self):
+        try:
+            self.port_gpp_4323 = self.comboBox_gpp_4323.currentText()
+            rm = pyvisa.ResourceManager()
+            items = rm.list_resources()  # Lists the connected VISA devices
+            if (not self.comboBox_gpp_4323.currentText() in items) or (self.comboBox_gpp_4323.currentText() == ""):  # Checks if the device is connected
+                QtWidgets.QMessageBox.information(self, 'Error', 'Wrong choice')
+                self.populate_gpp_4323_combobox()
+                self.port_gpp_4323 = None
+            else:  # If the device is connected, we do this
+                self.port_gpp_4323 = self.comboBox_gpp_4323.currentText()
+                self.gpp_4323 = rm.open_resource(self.port_gpp_4323)
+                print(self.gpp_4323.query('*IDN?'))
+                try:
+                    self.gpp_4323_tension_slider.setEnabled(True)   # Allow the user to use the slider
+                    self.brightness_slider.setEnabled(True)   # Allow the user to use the slider
+                    self.gpp_4323.write(f'ISET1:0.03')    # Fixes the maximum current so we can have a tension
+                    self.gpp_4323.write(f'ISET2:0.03')    # Fixes the maximum current so we can have a tension
+                    self.gpp_4323.write(f'ISET4:0.03')  # Fixes the maximum current so we can have a tension
+                    self.gpp_4323.write(f':ALLOUTON')  # Enables every channel
+                except Exception as e:
+                    QtWidgets.QMessageBox.information(self, 'Error', f"Couldn't connect to the instrument : {e}")
+                    self.gpp_4323_tension_slider.setEnabled(False)  # Prevent the user from using the slider
+                    self.brightness_slider.setEnabled(False)  # Prevent the user from using the slider
+                    self.populate_gpp_4323_combobox()
+                    self.port_gpp_4323 = None
+
+        except Exception as e:
+            QtWidgets.QMessageBox.information(self, 'Error', f"Connect_to_gpp_4323 function returned : {e}")
+
+    # Connection help button
+    def gpp_4323_help(self):
+        QtWidgets.QMessageBox.information(self, 'Help',
+                                          'Connect both CH1(-) and CH2(-) together and take your output between CH1(+) and CH2(+).\nConnect the electron detector to CH4.')
+
+    #########################################################################################
+    # Sliders part
+    def gpp_2323_slider_changed(self):
+        try:
+            tension = self.gpp_2323_tension_slider.value()  # Gets the value of the slider
+            self.label_current_tension_gpp_2323.setText(f'Supply voltage (V) : {str(tension)}')   # Shows to the user the current tension
+            if tension >= 0:
+                self.gpp_2323.write(f'VSET1:{tension}')
+                self.gpp_2323.write(f'VSET2:0')
+            else:
+                self.gpp_2323.write(f'VSET1:0')
+                self.gpp_2323.write(f'VSET2:{-tension}')
+        except Exception as e:
+            QtWidgets.QMessageBox.information(self, 'Error', f"Gpp_2323_slider_changed returned : {e}")
+
+    def gpp_4323_tension_slider_changed(self):
+        try:
+            tension = self.gpp_4323_tension_slider.value()  # Gets the value of the slider
+            self.label_current_tension_gpp_4323.setText(f'Supply voltage (V) : {str(tension)}')   # Shows to the user the current tension
+            if tension >= 0:
+                self.gpp_4323.write(f'VSET1:{tension}')
+                self.gpp_4323.write(f'VSET2:0')
+            else:
+                self.gpp_4323.write(f'VSET1:0')
+                self.gpp_4323.write(f'VSET2:{-tension}')
+        except Exception as e:
+            QtWidgets.QMessageBox.information(self, 'Error', f"Gpp_4323_slider_changed returned : {e}")
+
+    def gpp_4323_brightness_slider_changed(self):
+        try:
+            tension = self.brightness_slider.value()  # Gets the value of the slider
+            self.label_current_brightness.setText(f'Brightness tension (V) : {str(tension)}')   # Shows to the user the current tension
+            self.gpp_4323.write(f'VSET4:{tension}')
+        except Exception as e:
+            QtWidgets.QMessageBox.information(self, 'Error', f"Gpp_4323_brightness_slider_changed returned : {e}")
+
+    #########################################################################################
+
+
+# Starts the interface
+def run_interface():
+    try:
+        app = QtWidgets.QApplication(sys.argv)  # Create a Qt application
+        window = MyWindow()  # Create an instance of MyWindow
+        window.show()  # Show the window
+        sys.exit(app.exec())  # Start the application event loop
+    except Exception as e:
+        QtWidgets.QMessageBox.information(self, 'Error', f"Run_interface returned : {e}")
+
+
+# Main function
+if __name__ == '__main__':
+    run_interface()
