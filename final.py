8bda5920b68371e2479e8e834f25f0070cc55131
NeoVander <neo9295@gmail.com>
2023-11-30 19:27:59 +0100

    Multi-threading to control the power supplies

--- final.py
+++ final.py
@@ -2,10 +2,10 @@ import nidaqmx
 from nidaqmx.constants import TerminalConfiguration, AcquisitionType
 import sys
 from PyQt6 import QtCore, QtWidgets, uic
-from PyQt6.QtCore import QTimer
-import pyqtgraph
-import numpy as np
+from PyQt6.QtCore import QThread
+from PyQt6.QtWidgets import QApplication, QMainWindow
 import pyvisa
+import traceback
 
 # Load the UI file created with QT Designer
 Ui_MainWindow, QtBaseClass = uic.loadUiType("Interface_finale.ui")
@@ -13,7 +13,7 @@ Ui_MainWindow, QtBaseClass = uic.loadUiType("Interface_finale.ui")
 
 # Main class for the interface
 class MyWindow(QtWidgets.QMainWindow, Ui_MainWindow):
-    data_acquired = QtCore.pyqtSignal(list)  # Signal to emit acquired data
+    listChanged = QtCore.pyqtSignal(list)  # Signal to emit acquired data
 
     # Initialize the interface
     def __init__(self):
@@ -43,6 +43,8 @@ class MyWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         self.port_gpp_4323 = None
         self.gpp_2323 = None
         self.gpp_4323 = None
+        self.update_gpp_2323_thread = None
+        self.update_gpp_4323_thread = None
 
         # Initialize the comboBoxes
         self.populate_dev_combobox()
@@ -113,7 +115,8 @@ class MyWindow(QtWidgets.QMainWindow, Ui_MainWindow):
             self.port_gpp_2323 = self.comboBox_gpp_2323.currentText()
             rm = pyvisa.ResourceManager()
             items = rm.list_resources()  # Lists the connected VISA devices
-            if (not self.comboBox_gpp_2323.currentText() in items) or (self.comboBox_gpp_2323.currentText() == ""):  # Checks if the device is connected
+            if (not self.comboBox_gpp_2323.currentText() in items) or (
+                    self.comboBox_gpp_2323.currentText() == ""):  # Checks if the device is connected
                 QtWidgets.QMessageBox.information(self, 'Error', 'Wrong choice')
                 self.populate_gpp_2323_combobox()
                 self.port_gpp_2323 = None
@@ -122,9 +125,9 @@ class MyWindow(QtWidgets.QMainWindow, Ui_MainWindow):
                 self.gpp_2323 = rm.open_resource(self.port_gpp_2323)
                 print(self.gpp_2323.query('*IDN?'))
                 try:
-                    self.gpp_2323_tension_slider.setEnabled(True)   # Allow the user to use the slider
-                    self.gpp_2323.write(f'ISET1:0.03')    # Fixes the maximum current so we can have a tension
-                    self.gpp_2323.write(f'ISET2:0.03')    # Fixes the maximum current so we can have a tension
+                    self.gpp_2323_tension_slider.setEnabled(True)  # Allow the user to use the slider
+                    self.gpp_2323.write(f'ISET1:0.03')  # Fixes the maximum current so we can have a tension
+                    self.gpp_2323.write(f'ISET2:0.03')  # Fixes the maximum current so we can have a tension
                     self.gpp_2323.write(f':ALLOUTON')  # Enables every channel
                 except Exception as e:
                     QtWidgets.QMessageBox.information(self, 'Error', f"Couldn't connect to the instrument : {e}")
@@ -160,7 +163,8 @@ class MyWindow(QtWidgets.QMainWindow, Ui_MainWindow):
             self.port_gpp_4323 = self.comboBox_gpp_4323.currentText()
             rm = pyvisa.ResourceManager()
             items = rm.list_resources()  # Lists the connected VISA devices
-            if (not self.comboBox_gpp_4323.currentText() in items) or (self.comboBox_gpp_4323.currentText() == ""):  # Checks if the device is connected
+            if (not self.comboBox_gpp_4323.currentText() in items) or (
+                    self.comboBox_gpp_4323.currentText() == ""):  # Checks if the device is connected
                 QtWidgets.QMessageBox.information(self, 'Error', 'Wrong choice')
                 self.populate_gpp_4323_combobox()
                 self.port_gpp_4323 = None
@@ -169,10 +173,10 @@ class MyWindow(QtWidgets.QMainWindow, Ui_MainWindow):
                 self.gpp_4323 = rm.open_resource(self.port_gpp_4323)
                 print(self.gpp_4323.query('*IDN?'))
                 try:
-                    self.gpp_4323_tension_slider.setEnabled(True)   # Allow the user to use the slider
-                    self.brightness_slider.setEnabled(True)   # Allow the user to use the slider
-                    self.gpp_4323.write(f'ISET1:0.03')    # Fixes the maximum current so we can have a tension
-                    self.gpp_4323.write(f'ISET2:0.03')    # Fixes the maximum current so we can have a tension
+                    self.gpp_4323_tension_slider.setEnabled(True)  # Allow the user to use the slider
+                    self.brightness_slider.setEnabled(True)  # Allow the user to use the slider
+                    self.gpp_4323.write(f'ISET1:0.03')  # Fixes the maximum current so we can have a tension
+                    self.gpp_4323.write(f'ISET2:0.03')  # Fixes the maximum current so we can have a tension
                     self.gpp_4323.write(f'ISET4:0.03')  # Fixes the maximum current so we can have a tension
                     self.gpp_4323.write(f':ALLOUTON')  # Enables every channel
                 except Exception as e:
@@ -195,36 +199,75 @@ class MyWindow(QtWidgets.QMainWindow, Ui_MainWindow):
     def gpp_2323_slider_changed(self):
         try:
             tension = self.gpp_2323_tension_slider.value()  # Gets the value of the slider
-            self.label_current_tension_gpp_2323.setText(f'Supply voltage (V) : {str(tension)}')   # Shows to the user the current tension
-            if tension >= 0:
-                self.gpp_2323.write(f'VSET1:{tension}')
-                self.gpp_2323.write(f'VSET2:0')
-            else:
-                self.gpp_2323.write(f'VSET1:0')
-                self.gpp_2323.write(f'VSET2:{-tension}')
+            self.label_current_tension_gpp_2323.setText(
+                f'Supply voltage (V) : {str(tension)}')  # Shows to the user the current tension
+            self.update_gpp_2323_thread = GPP2323UpdateThread(self.gpp_2323, tension)
+            self.update_gpp_2323_thread.finished.connect(self.deleteLater)  # Cleans the thread when done
+            self.update_gpp_2323_thread.start()  # Starts the thread
         except Exception as e:
             QtWidgets.QMessageBox.information(self, 'Error', f"Gpp_2323_slider_changed returned : {e}")
 
     def gpp_4323_tension_slider_changed(self):
         try:
             tension = self.gpp_4323_tension_slider.value()  # Gets the value of the slider
-            self.label_current_tension_gpp_4323.setText(f'Supply voltage (V) : {str(tension)}')   # Shows to the user the current tension
-            if tension >= 0:
-                self.gpp_4323.write(f'VSET1:{tension}')
-                self.gpp_4323.write(f'VSET2:0')
-            else:
-                self.gpp_4323.write(f'VSET1:0')
-                self.gpp_4323.write(f'VSET2:{-tension}')
+            self.label_current_tension_gpp_4323.setText(
+                f'Supply voltage (V) : {str(tension)}')  # Shows to the user the current tension
+            self.update_gpp_4323_thread = GPP4323UpdateThread(self.gpp_4323, tension)
+            self.update_gpp_4323_thread.finished.connect(self.deleteLater)  # Cleans the thread when done
+            self.update_gpp_4323_thread.start()  # Starts the thread
         except Exception as e:
             QtWidgets.QMessageBox.information(self, 'Error', f"Gpp_4323_slider_changed returned : {e}")
 
     def gpp_4323_brightness_slider_changed(self):
         try:
             tension = self.brightness_slider.value()  # Gets the value of the slider
-            self.label_current_brightness.setText(f'Brightness tension (V) : {str(tension)}')   # Shows to the user the current tension
-            self.gpp_4323.write(f'VSET4:{tension}')
+            self.label_current_brightness.setText(
+                f'Brightness tension (V) : {str(tension)}')  # Shows to the user the current tension
+            self.gpp_4323.write(f'VSET4:{self.tension}')
         except Exception as e:
             QtWidgets.QMessageBox.information(self, 'Error', f"Gpp_4323_brightness_slider_changed returned : {e}")
+
+    #########################################################################################
+    # Multithreading part
+
+    # Clean up the used thread resources
+    def thread_cleanup(self):
+        sender = self.sender()  # Retrieves the object that emitted the signal (in this case, the finished thread)
+        if sender:
+            sender.deleteLater()  # Safely deletes the thread object to free up resources
+
+
+# Thread Class for Updating GPP2323's tension (-32V ; +32V)
+class GPP2323UpdateThread(QThread):
+    def __init__(self, gpp_2323, tension):
+        super(GPP2323UpdateThread, self).__init__()  # Initialize the QThread parent class
+        self.gpp_2323 = gpp_2323  # Stores the reference to the GPP2323 device object
+        self.tension = tension  # Stores the tension value to be set
+
+    def run(self):
+        if self.tension >= 0:
+            self.gpp_2323.write(f'VSET1:{self.tension}')
+            self.gpp_2323.write(f'VSET2:0')
+        else:
+            self.gpp_2323.write(f'VSET1:0')
+            self.gpp_2323.write(f'VSET2:{-self.tension}')
+
+
+# Thread Class for Updating GPP4323's tension (-32V ; +32V)
+class GPP4323UpdateThread(QThread):
+    def __init__(self, gpp_4323, tension, parent=None):
+        super(GPP4323UpdateThread, self).__init__(parent)  # Initialize the QThread parent class
+        self.gpp_4323 = gpp_4323  # Stores the reference to the GPP4323 device object
+        self.tension = tension  # Stores the tension value to be set
+
+    def run(self):
+        # Sends the value requested by the user to the device
+        if self.tension >= 0:
+            self.gpp_4323.write(f'VSET1:{self.tension}')
+            self.gpp_4323.write(f'VSET2:0')
+        else:
+            self.gpp_4323.write(f'VSET1:0')
+            self.gpp_4323.write(f'VSET2:{-self.tension}')
 
     #########################################################################################
 
